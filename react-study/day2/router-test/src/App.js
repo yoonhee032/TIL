ab9e924e4506eed9622ecb7cb70819536b490b9a
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-06-07 13:27:36 +0900

    desc : 브라우저 히스토리 API 사용
    브라우저 히스토리 API를 사용하여 단일 페이지 어플리케이션 작성

--- react-study/day2/router-test/src/App.js
+++ react-study/day2/router-test/src/App.js
@@ -1,23 +1,51 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    pageName: "",
+  };
   componentDidMount() {
-    window.onpopstate = function (event) {
-      console.log(`location: ${document.location}, state : ${event.state}`);
+    window.onpopstate = (event) => {
+      this.onChangePage(event.state);
     };
   }
+  onChangePage = (pageName) => {
+    this.setState({ pageName });
+  };
+  onClick1 = () => {
+    const pageName = "page1";
+    window.history.pushState(pageName, "", "/page1");
+    this.onChangePage(pageName);
+  };
+  onClick2 = () => {
+    const pageName = "page2";
+    window.history.pushState(pageName, "", "/page2");
+    this.onChangePage(pageName);
+  };
   render() {
+    const { pageName } = this.state;
     return (
       <div>
-        <button onClick={() => window.history.pushState("v1", "", "/page1")}>
-          page1
-        </button>
-        <button onClick={() => window.history.pushState("v2", "", "/page2")}>
-          page2
-        </button>
+        <button onClick={this.onClick1}>page1</button>
+        <button onClick={this.onClick2}>page2</button>
+        {!pageName && <Home />}
+        {pageName === "page1" && <Page1 />}
+        {pageName === "page2" && <Page2 />}
       </div>
     );
   }
+}
+
+function Home() {
+  return <h2>여기는 홈페이지입니다. 원하는 페이지 버튼을 클릭하세요</h2>;
+}
+
+function Page1() {
+  return <h2>여기는 페이지1입니다.</h2>;
+}
+
+function Page2() {
+  return <h2>여기는 페이지2입니다.</h2>;
 }
 
 export default App;
