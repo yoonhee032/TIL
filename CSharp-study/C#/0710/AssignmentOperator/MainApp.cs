fa93d391000b6a8b43912ee68fc3eb266e75d072
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-07-10 20:10:01 +0900

    desc : 비트 논리 연산자, 할당 연산자, null 병합 연산자

---
+++ CSharp-study/C#/0710/AssignmentOperator/MainApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AssignmentOperator
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = 100;
+            Console.WriteLine($"a = 100 : {a}");
+            a += 90;
+            Console.WriteLine($"a = 100 : {a}");
+            a -= 80;
+            Console.WriteLine($"a = 100 : {a}");
+            a *= 70;
+            Console.WriteLine($"a = 100 : {a}");
+            a /= 60;
+            Console.WriteLine($"a = 100 : {a}");
+            a %= 50;
+            Console.WriteLine($"a = 100 : {a}");
+            a &= 40;
+            Console.WriteLine($"a = 100 : {a}");
+            a |= 30;
+            Console.WriteLine($"a = 100 : {a}");
+            a ^= 20;
+            Console.WriteLine($"a = 100 : {a}");
+            Console.WriteLine($"a = 100 : {a}");
+
+            a <<= 10;
+            Console.WriteLine($"a = 100 : {a}");
+            a >>= 1;
+            Console.WriteLine($"a = 100 : {a}");
+
+        }
+    }
+}
